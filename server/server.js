916d70a73a70c546ae0704202a7ab51ec9e26e4d
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-18 23:51:22 +0530

    Start an Express server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import { config as configDotenv } from "dotenv";
+import app from "./app.js";
+import connectDatabase from "./config/db.js";
+
+// Load environment variables from .env file
+configDotenv();
+
+const port = process.env.PORT || 7000;
+
+// Start the server
+app.listen(port, async () => {
+  await connectDatabase();
+  console.log(`Server is running on http://localhost:${port}`);
+});

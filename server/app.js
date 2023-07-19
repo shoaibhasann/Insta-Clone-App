047e9555c445dd7b896879624eacdd8a385d2c8d
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-19 14:38:45 +0530

    Setup Express App

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoutes from "./routes/user.route.js";
+
+const app = express();
+
+// middleware setup
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5500',
+    credentials: true,
+}));
+
+
+// routes
+app.use('/',userRoutes);
+
+export default app;
